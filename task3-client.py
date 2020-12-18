c8bca3ff3b99922be2e7ed379f4e57f5030c7cad
He YangLiang <46257891+heyl18@users.noreply.github.com>
2020-12-18 21:52:55 +0800

    Update task3-client.py

--- task3-client.py
+++ task3-client.py
@@ -20,9 +20,33 @@ time3 = record_file(5,'recv.wav',True)
 # 接收小于 1024 字节的数据
 time1 = s.recv(1024)
 s.close()
-data, nframes, framerate = open_wave_file('recv.wav')
-plt.plot(data)
+sig_rec, nframes, framerate = open_wave_file('recv.wav')
+plt.plot(sig_rec)
 plt.show()
-
+f = 6000
+fs = 48000
+duration = 0.0125
+symbol_len = int(duration * fs)
+size_data_len = 8
+pre_data_len = 10
+threshold = 0.35
+pre_threshold = 0.3
+max_symbol_sum = 0
+first_impulse = 0  # 取出impulse 第一个起始位置
+for i in range(0, len(sig_rec) - symbol_len):
+    symbol_sum = np.sum(np.abs(sig_rec[i:i + symbol_len]))
+    if symbol_sum > max_symbol_sum:
+        max_symbol_sum = symbol_sum
+    print(max_symbol_sum)
+for i in range(0, len(sig_rec) - symbol_len):
+    symbol_sum = np.sum(np.abs(sig_rec[i:i + symbol_len]))
+    if symbol_sum > pre_threshold * max_symbol_sum:
+        max_sum = symbol_sum
+        for j in range(i - int(symbol_len / 2), i + int(symbol_len)):
+            now_sum = np.sum(np.abs(sig_rec[j:j + symbol_len]))
+            if now_sum > max_sum:
+                max_sum = now_sum
+                first_impulse = j
+        break
 print (time1.decode('utf-8'))
-print(float(time3)-float(time1.decode('utf-8').replace('\r\n','')))
+print(float(time3)-float(time1.decode('utf-8').replace('\r\n','')) + float(first_umpluse/fs))
